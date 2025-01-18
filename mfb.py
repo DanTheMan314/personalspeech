a3b871d1f0506210b349e9fa4d5d8429805511dc
DanTheMan314 <dankoshyg@gmail.com>
2025-01-18 14:01:41 +0530

    implementation begins

---
+++ mfb.py
@@ -0,0 +1,35 @@
+#Setup
+
+import numpy
+import scipy.io.wavfile
+from scipy.fftpack import dct
+
+sample_rate, signal = scipy.io.wavfile.read('OSR_us_000_0010_8k.wav')  # File assumed to be in the same directory
+signal = signal[0:int(3.5 * sample_rate)]  # Keep the first 3.5 seconds
+
+#Pre-emphasis
+
+pre_emphasis = 0.97
+emphasized_signal = numpy.append(signal[0], signal[1:] - pre_emphasis * signal[:-1])
+
+#Framing
+
+frame_size = 0.025
+frame_stride = 0.01
+frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate  # Convert from seconds to samples
+signal_length = len(emphasized_signal)
+frame_length = int(round(frame_length))
+frame_step = int(round(frame_step))
+num_frames = int(numpy.ceil(float(numpy.abs(signal_length - frame_length)) / frame_step))  # Make sure that we have at least 1 frame
+
+pad_signal_length = num_frames * frame_step + frame_length
+z = numpy.zeros((pad_signal_length - signal_length))
+pad_signal = numpy.append(emphasized_signal, z) # Pad Signal to make sure that all frames have equal number of samples without truncating any samples from the original signal
+
+indices = numpy.tile(numpy.arange(0, frame_length), (num_frames, 1)) + numpy.tile(numpy.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
+frames = pad_signal[indices.astype(numpy.int32, copy=False)]
+
+#Window
+
+frames *= numpy.hamming(frame_length)
+# frames *= 0.54 - 0.46 * numpy.cos((2 * numpy.pi * n) / (frame_length - 1))  # Explicit Implementation **
